651a42a18ea43edb76996993de9303f1dbf402f1
xtwgok3 <xtwgok@gmail.com>
2024-10-08 12:58:56 -0300

    asd

--- resources/views/livewire/product-list.blade.php
+++ resources/views/livewire/product-list.blade.php
@@ -1,41 +1,40 @@
 <div>
     @if (request()->is('home') && preg_match('/mobile/i', request()->header('User-Agent')))
-        <style>
-            #pdr {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                text-align: center;
-            }
-        </style>
+    <style>
+        #pdr {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+    </style>
     @else
-        <style>
-            #pdr {
-                display: flex;
-                flex-wrap: wrap;
-                /* Para permitir que los elementos se ajusten */
-                justify-content: space-between;
-                /* Espaciado uniforme entre elementos */
-                align-items: stretch;
-                /* Asegura que todos los elementos tengan la misma altura */
-            }
+    <style>
+        #pdr {
+            display: flex;
+            flex-wrap: wrap;
+            /* Para permitir que los elementos se ajusten */
+            justify-content: space-between;
+            /* Espaciado uniforme entre elementos */
+            align-items: stretch;
+        }
 
-            H6 {
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-            }
-        </style>
+        H6 {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+    </style>
     @endif
 
     <div class="container">
         <div class="row justify-content-center">
 
             @if (session('success'))
-                <div class="alert alert-primary" role="alert">
-                    {{ session('success') }}
-                </div>
+            <div class="alert alert-primary" role="alert">
+                {{ session('success') }}
+            </div>
             @endif
 
             <form wire:submit.prevent="filterProducts">
@@ -44,7 +43,7 @@
                         <select wire:model="selectedCategory" class="form-select">
                             <option value="">Todas las Categorías</option>
                             @foreach ($categories as $category)
-                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -61,39 +60,39 @@
 
             <div class="col-md-9" ondragstart="return false;">
                 @if (auth()->user()->isAdmin())
-                    <div class="container mb-3">
-                        <div class="row d-flex justify-content-center">
-                            <div id="btnpdr" class="col-sm-2 text-end">
-                                <a href="{{ route('products.create') }}" class="btn btn-primary">Crear Producto</a>
-                            </div>
-                            <div class="col-sm-2 text-end mt-3 mt-sm-0">
-                                <a href="{{ route('categories.index') }}" class="btn btn-secondary">Editar Categoria</a>
-                            </div>
+                <div class="container mb-3">
+                    <div class="row d-flex justify-content-center">
+                        <div id="btnpdr" class="col-sm-2 text-end">
+                            <a href="{{ route('products.create') }}" class="btn btn-primary">Crear Producto</a>
+                        </div>
+                        <div class="col-sm-2 text-end mt-3 mt-sm-0">
+                            <a href="{{ route('categories.index') }}" class="btn btn-secondary">Editar Categoria</a>
                         </div>
                     </div>
+                </div>
                 @endif
             </div>
 
             @if ($products->count() > 0)
-                <div id="pdr" class="col-md-9" ondragstart="return false;">
+            <div id="pdr" class="col-md-9" ondragstart="return false;">
 
-                    <div class="row w-100">
-                        @each('products._product', $products, 'product')
-                    </div>
-
-                    <div class="row">
-                        {{ $products->links() }}
-                    </div>
+                <div class="row w-100">
+                    @each('products._product', $products, 'product')
                 </div>
+
+                <div class="row">
+                    {{ $products->links() }}
+                </div>
+            </div>
             @else
-                <div class="col-md-9" ondragstart="return false;">
-                    <li class="list-group-item">
-                        <div class="text-center"
-                            style="user-select: none; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); padding: 7px; border-radius: 6px;">
-                            <strong>No se encontró el producto.</strong>
-                        </div>
-                    </li>
-                </div>
+            <div class="col-md-9" ondragstart="return false;">
+                <li class="list-group-item">
+                    <div class="text-center"
+                        style="user-select: none; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); padding: 7px; border-radius: 6px;">
+                        <strong>No se encontró el producto.</strong>
+                    </div>
+                </li>
+            </div>
             @endif
 
         </div>
